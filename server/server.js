5c3c65bffc15b51f318f9e136b51c774b8f6422d
Prateek Redkar <prateek.redkar@spit.ac.in>
2024-03-14 20:22:03 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const errorMiddleware = require("./middlewares/error-middleware");
 
 // Handling Cors policy
 const corsOptions = {
-    origin: "https://my-portfolio-client-rouge.vercel.app",
+    origin: "http://localhost:5173",
     methods: "GET, PUT, POST, DELETE, HEAD, PATCH",
     credentials: true
 };
